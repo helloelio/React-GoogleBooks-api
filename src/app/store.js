ffccede1a55bbe03c354b8ce436fe4aaa6fa422d
hellelio <freadwantblossom@gmail.com>
2021-11-21 14:11:20 +0300

    [test] Изменил глобальный store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import booksAPI from "./booksAPI";
+import {categorieParameter} from './categorieParameter'
+import {searchParameter} from "./searchParameter";
+import {booksList} from "./booksList";
+import {totalBooks} from "./totalBooks";
+import {bookItem} from "./bookItem";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+    reducer: {
+        booksAPI,
+        booksList,
+        bookItem,
+        totalBooks,
+        categorieParameter,
+        searchParameter,
+        counter: counterReducer,
+    },
 });
