c84954e7291a729d1824e13334564b2f35e017d8
hellelio <freadwantblossom@gmail.com>
2021-11-21 09:19:42 +0300

    [test] Изменил компонент BookItems добавив счетчик книг и кнопку для загрузки книг

--- src/features/books/Books.js
+++ src/features/books/Books.js
@@ -4,26 +4,37 @@ import notFound from './img/not-found.png';
 import {
     Link
 } from "react-router-dom";
+import BooksCounter from "./BooksCounter";
 
 function BookItems(props) {
     return (
-        props.result.map(book => (
-                <nav>
-                    <Link to={`book/${book.id}`} onClick={props.test} id={book.id} key={book.id}>
-                        <div className='book'>
-                            <img id={book.id} className='book__image'
-                                 src={_.get(book, "volumeInfo.imageLinks.thumbnail", `${notFound}`)}
-                                 alt='asd'
-                            />
-                            <div className='book__description'>
-                                <p className='book__categories'>{_.get(book, "volumeInfo.categories", '')}</p>
-                                <p className='book__title'>{_.get(book, "volumeInfo.title", '')}</p>
-                                <p className='book__authors'>{_.get(book, "volumeInfo.authors", '')}</p>
+        <div className='book-items'>
+            <BooksCounter total={props.total}/>
+            {
+                props.result.map(book => (
+                    <nav>
+                        <Link to={`book/${book.id}`} onClick={props.getBook} id={book.id} key={book.id}>
+                            <div className='book'>
+                                <img id={book.id} className='book__image'
+                                     src={_.get(book, "volumeInfo.imageLinks.thumbnail", `${notFound}`)}
+                                     alt='asd'
+                                />
+                                <div className='book__description'>
+                                    <p className='book__categories'>{_.get(book, "volumeInfo.categories", '')}</p>
+                                    <p className='book__title'>{_.get(book, "volumeInfo.title", '')}</p>
+                                    <p className='book__authors'>{_.get(book, "volumeInfo.authors", '')}</p>
+                                </div>
                             </div>
-                        </div>
-                    </Link>
-                </nav>
-        ))
+                        </Link>
+                    </nav>
+                ))
+            }
+            <div className='load-button'>
+                {props.result.length > 0 &&
+                <button onClick={'handleLoadBooks'} className='load-books'> Load more...</button>
+                }
+            </div>
+        </div>
     )
 }
 
