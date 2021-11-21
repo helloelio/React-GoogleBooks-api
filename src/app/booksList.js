ea9332a173b37ce54a28bc1c082fb21d6a7dd67d
hellelio <freadwantblossom@gmail.com>
2021-11-21 15:15:21 +0300

    [test] Добавил action для подгрузки новых книг

--- src/app/booksList.js
+++ src/app/booksList.js
@@ -1,10 +1,9 @@
-import _ from 'lodash'
-
 const defaultState = {
     books: []
 }
 
 const GET_BOOKS = 'GET_BOOKS';
+const GET_LOAD_BOOKS = 'GET_LOAD_BOOKS'
 const GET_FILTER_BOOKS = 'GET_FILTER_BOOKS';
 
 export const booksList = (state = defaultState, action) => {
@@ -15,10 +14,13 @@ export const booksList = (state = defaultState, action) => {
         case GET_FILTER_BOOKS:
             return {...state, books: action.payload.data}
             break;
+        case GET_LOAD_BOOKS:
+            return {...state, books: [...state.books, ...action.payload]}
         default:
             return {...state}
     }
 }
 
 export const getBooksAction = (payload) => ({type: GET_BOOKS, payload});
-export const getFilterBooksAction = (payload) => ({type:GET_FILTER_BOOKS, payload})
+export const loadBooksAction = (payload) => ({type: GET_LOAD_BOOKS, payload});
+export const getFilterBooksAction = (payload) => ({type:GET_FILTER_BOOKS, payload});
