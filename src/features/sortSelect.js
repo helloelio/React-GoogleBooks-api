910c371812e054897d1d7fade960c0a1d3e39c95
hellelio <freadwantblossom@gmail.com>
2021-11-22 10:36:01 +0300

    [test] Рефактор

--- src/features/sortSelect.js
+++ src/features/sortSelect.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function sortSelect(props) {
     return (
-        <div className='search__selects-sort'>
+        <div className="search__selects-sort">
             <label htmlFor="sorting">Sorting by</label>
             <select name="sorting" id="sorting" onChange={props.sortBooks}>
-                <option value="relevance ">relevance</option>
+                <option value="relevance">relevance</option>
                 <option value="newest">newest</option>
             </select>
         </div>
-    )
+    );
 }
 
 export default sortSelect;
