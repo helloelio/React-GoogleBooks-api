6b3923419e9746a227eacc4a1b7990eb0adf5517
hellelio <freadwantblossom@gmail.com>
2021-11-22 10:36:23 +0300

    [test] Рефактор

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,12 @@
-import {configureStore} from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import {configureStore} from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 import booksAPI from "./booksAPI";
-import {categorieParameter} from './categorieParameter'
+import {categorieParameter} from "./categorieParameter";
 import {searchParameter} from "./searchParameter";
 import {booksList} from "./booksList";
 import {totalBooks} from "./totalBooks";
 import {bookItem} from "./bookItem";
+import {sortParameter} from "./sortParameter";
 
 export const store = configureStore({
     reducer: {
@@ -14,6 +15,7 @@ export const store = configureStore({
         bookItem,
         totalBooks,
         categorieParameter,
+        sortParameter,
         searchParameter,
         counter: counterReducer,
     },
