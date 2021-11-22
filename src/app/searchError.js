0c9ce8f1420b00bb3a7c7e736e0d5bee708e4864
hellelio <freadwantblossom@gmail.com>
2021-11-22 22:28:34 +0300

    [test] Добавил store для обработки компонента ошибки

---
+++ src/app/searchError.js
@@ -0,0 +1,16 @@
+const defaultState = {
+    error: false,
+}
+
+const GET_ERROR = 'GET_ERROR';
+
+export const searchError = (state = defaultState, action) => {
+    switch (action.type) {
+        case GET_ERROR:
+            return {...state, error: action.payload}
+        default:
+            return {...state}
+    }
+}
+
+export const errorAction = (payload) => ({type: GET_ERROR, payload})
