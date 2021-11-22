4986c5d8155fb9c80786ee6e8c97d5dff58a1677
hellelio <freadwantblossom@gmail.com>
2021-11-22 22:28:15 +0300

    [test] Добавил компоненту ошибки

---
+++ src/features/error/error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function errorMessage () {
+    return (
+        <div className='error-block'>
+            <p className='error-text'>You must enter the title of the book.</p>
+        </div>
+    )
+}
+
+export default errorMessage;
