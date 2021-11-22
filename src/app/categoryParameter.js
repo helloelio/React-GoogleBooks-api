898cb7af7931b0527ecb329471a62ab2e92a027c
hellelio <freadwantblossom@gmail.com>
2021-11-22 17:45:52 +0300

    [test] Ренейминг

---
+++ src/app/categoryParameter.js
@@ -0,0 +1,17 @@
+const defaultState = {
+    category: 'all',
+}
+
+const GET_CATEGORY = 'GET_CATEGORY';
+
+export const categoryParameter = (state = defaultState, action) => {
+    switch (action.type) {
+        case GET_CATEGORY:
+            return {...state, category: action.payload}
+        default:
+            return {...state}
+
+    }
+}
+
+export const getCategorieParameterAction = (payload) => ({type: GET_CATEGORY, payload});
